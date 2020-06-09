548bc4948f54c4c58ba5d0d82fd56e05ed1347e7
Spencer Taft <spencerp@uvic.ca>
2020-06-08 21:08:41 -0700

    Starting to play with file creation

--- app/src/main/java/com/example/racinglumber/fileManageActivity.java
+++ app/src/main/java/com/example/racinglumber/fileManageActivity.java
@@ -2,7 +2,9 @@ package com.example.racinglumber;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -32,6 +34,60 @@ public class fileManageActivity extends Activity implements BottomNavigationView
         bottomNavigationView.setSelectedItemId(R.id.bottom_nav_save_button);
     }
 
+    private static final int OPEN_DIRECTORY_REQUEST_CODE = 1;
+    void openDirectory() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+        startActivityForResult(intent, OPEN_DIRECTORY_REQUEST_CODE);
+    }
+///////////////////////////////////////////////////////FROM ANDROID TUTORIAL
+    ////////////////////////////////////////////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    // Request code for creating a PDF document.
+    private static final int CREATE_FILE = 1;
+    private void createFile() { //Uri pickerInitialUri
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+        intent.putExtra(Intent.EXTRA_TITLE, "invoice.pdf");
+
+        // Optionally, specify a URI for the directory that should be opened in
+        // the system file picker when your app creates the document.
+        //intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, CREATE_FILE);
+    }
+
+    // Request code for selecting a PDF document.
+    private static final int PICK_PDF_FILE = 2;
+    private void openFile() { //Uri pickerInitialUri
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+
+        // Optionally, specify a URI for the file that should appear in the
+        // system file picker when it loads.
+        //intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, PICK_PDF_FILE);
+    }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode,
+//                                 Intent resultData) {
+//    //    if (requestCode == your-request-code
+//    //            && resultCode == Activity.RESULT_OK) {
+//            // The result data contains a URI for the document or directory that
+//            // the user selected.
+//            Uri uri = null;
+//            if (resultData != null) {
+//                uri = resultData.getData();
+//                // Perform operations on the document using its URI.
+//            }
+//    //    }
+//    }
+///////////////////////////////////////////////////////FROM ANDROID TUTORIAL///
+    ////////////////////////////////////////////////////////////////<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
     //////////////////////////User Interface Functions//////////////////////////
 
     @Override
@@ -40,6 +96,11 @@ public class fileManageActivity extends Activity implements BottomNavigationView
         //do something
         int test = 1;
         test = 2;
+        this.createFile();
+ //       this.openFile();
+        //this.getExternalFilesDir()
+
+        openDirectory();
     }
 
     @Override
