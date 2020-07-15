057793dbe7200e40985eb5e4ff05c73815be5b35
Spencer Taft <spencerp@uvic.ca>
2020-07-14 20:58:09 -0700

    Added google sdk for location to app

--- app/src/main/java/com/example/racinglumber/locationRecording.java
+++ app/src/main/java/com/example/racinglumber/locationRecording.java
@@ -3,6 +3,7 @@ package com.example.racinglumber;
 public class locationRecording {
 
     //private FusedLocationProviderClient fusedLocationClient;
+    private FusedLocationProviderClient fusedLocationClient;
 
     public void testFunc()
     {
