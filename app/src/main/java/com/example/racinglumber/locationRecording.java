70f011833284894459b2e0390449fc7b1623bb62
Spencer Taft <spencerp@uvic.ca>
2020-07-15 21:11:13 -0700

    Fixed google api add problem

--- app/src/main/java/com/example/racinglumber/locationRecording.java
+++ app/src/main/java/com/example/racinglumber/locationRecording.java
@@ -1,12 +1,17 @@
 package com.example.racinglumber;
 
+import android.app.Activity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
 public class locationRecording {
 
-    //private FusedLocationProviderClient fusedLocationClient;
     private FusedLocationProviderClient fusedLocationClient;
 
-    public void testFunc()
+    public void testFunc(Activity activity)
     {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity);
 
     }
 }
