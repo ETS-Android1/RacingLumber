b0ffc27a32f0e8a290aa65f99ee48c54637b32c4
Spencer Taft <shperks@gmail.com>
2021-03-20 08:27:06 -0700

    Added lateral and longitudal calculations

--- app/src/main/java/com/example/racinglumber/synthesizedData.java
+++ app/src/main/java/com/example/racinglumber/synthesizedData.java
@@ -4,43 +4,105 @@ import static com.example.racinglumber.dataStorage.dataArrayLen;
 
 //This class contains a set of synthesized latitudal, longitudal, etc. data using sensor fusion
 public class synthesizedData {
-    private static float[] lateralDataArray; //left (negative) right (positive acceleration)
+    private static float[] lateralDataArray; //left (negative) right (positive) acceleration
+    private static float[] longitudalDataArray; //backward (negative) forward (positive) acceleration
     private dataStorage recordedVars; //todo this should just be static?
+    private float forwardVectorX;
+    private float forwardVectorY;
 
     //custom constructor
     public synthesizedData()
     {
         lateralDataArray = new float[dataArrayLen];
         recordedVars = new dataStorage();
-        computeLateral();
+        computeForwardVector();
+        computeLateralLongitudalArrays();
     }
 
 
-    public void computeLateral()
+    public void computeForwardVector()
     {
-        int i;
-        double latitude;
-        double latitudeNext;
-        double longitude;
-        double longitudeNext;
-        double latDelta, longDelta;
+        /*Prototype is using 100Hz capture.  First 3 seconds is setup(putting in pocket), 3 seconds is averaging vector of kart going forward, then save*/
+        float xArray = 0.0f;
+        float yArray = 0.0f;
+        float xAvg, yAvg;
+        float magnitude;
 
-
-        for (i = 0; i < dataArrayLen-1; i++)
+        //todo could make delay configurable
+        for (int index = 300; index < 600; index++)//start at 300 to make it start at 3 seconds, 3* 100Hz
         {
-            //find GPS vector using current and next points
-            latitude = recordedVars.getGPSValueFromAccelDataIndex(true, i);
-            latitudeNext = recordedVars.getGPSValueFromAccelDataIndex(true, i+1);
-            longitude = recordedVars.getGPSValueFromAccelDataIndex(false, i);
-            longitudeNext = recordedVars.getGPSValueFromAccelDataIndex(false, i+1);
+            if (index < recordedVars.getDataArrayLen())
+            {
+                xArray += recordedVars.getSensorValue(dataStorage.Axis.X, dataStorage.RecordType.acceleration, index);
+                yArray += recordedVars.getSensorValue(dataStorage.Axis.Y, dataStorage.RecordType.acceleration, index);
+            }
+        }
 
-            latDelta = latitudeNext - latitude;
-            longDelta = longitudeNext - longitude;
+        //todo could use moving average algorithm to be more accurate
+        xAvg = xArray/300.0f;
+        yAvg = yArray/300.0f;
+        magnitude = (float) Math.sqrt((xAvg*xAvg)+(yAvg*yAvg));
 
-            //find angle between XY acceleration vectors and GPS vector
+        //normalize vector
+        forwardVectorX = xAvg/magnitude;
+        forwardVectorY = yAvg/magnitude;
+    }
 
-            //split latitude and longitude parts
+    public void computeLateralLongitudalArrays()
+    {
+        float xVal, yVal, xyMag, xyDotProduct, cosTheta, theta;
+
+        lateralDataArray = new float[recordedVars.getDataArrayLen()];
+        longitudalDataArray = new float[recordedVars.getDataArrayLen()];
+
+        //Instantiate lateral data array
+        for (int index = 0; index < recordedVars.getDataArrayLen(); index++)
+        {
+            //use dot product to get angle:
+
+            //new vector, get magnitude
+            xVal = recordedVars.getSensorValue(dataStorage.Axis.X, dataStorage.RecordType.acceleration, index);
+            yVal = recordedVars.getSensorValue(dataStorage.Axis.Y, dataStorage.RecordType.acceleration, index);
+            xyMag = (float) Math.sqrt((xVal*xVal)+(yVal*yVal));
+            //get dot product
+            xyDotProduct = (xVal*forwardVectorX) + (yVal*forwardVectorX);
+            //get cosine theta
+            cosTheta = xyDotProduct/xyMag;
+            //get angle between vectors
+            theta = (float) Math.acos(cosTheta);
+
+            //todo some of these signs might need to flip
+            //Get the lateral component
+            lateralDataArray[index] = xyMag*((float)Math.sin(theta));
+            //get the longitudal component
+            longitudalDataArray[index] = xyMag*cosTheta;
         }
     }
 
 }
+
+
+
+//        int i;
+//        double latitude;
+//        double latitudeNext;
+//        double longitude;
+//        double longitudeNext;
+//        double latDelta, longDelta;
+
+
+//        for (i = 0; i < dataArrayLen-1; i++)
+//        {
+//            //find GPS vector using current and next points
+//            latitude = recordedVars.getGPSValueFromAccelDataIndex(true, i);
+//            latitudeNext = recordedVars.getGPSValueFromAccelDataIndex(true, i+1);
+//            longitude = recordedVars.getGPSValueFromAccelDataIndex(false, i);
+//            longitudeNext = recordedVars.getGPSValueFromAccelDataIndex(false, i+1);
+//
+//            latDelta = latitudeNext - latitude;
+//            longDelta = longitudeNext - longitude;
+//
+//            //find angle between XY acceleration vectors and GPS vector
+//
+//            //split latitude and longitude parts
+//        }
